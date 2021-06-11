aecbe783067694475c67abd30a5bab1f8e9257b6
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-06-11 00:00:21 -0700

    l

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,16 +1,54 @@
+const htmltag = document.getElementsByTagName('html')[0]
+
+
 function navigation(){
-    let e = document.querySelector('.navigation').classList
-    e.toggle('closed')
+    document.querySelector('.navigation').classList.toggle('closed')
 }
-function copyToClipboard_hideblockedmessages_both() {
-    const str = document.querySelector('#hideblockedmessages .code[aria-type="both"]').innerText;
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
+let BtnEle = document.querySelectorAll('.copy').forEach(ee => (
+    ee.addEventListener("click", () => {
+        const copycontent = ee.getAttribute('copy')
+        const str = document.querySelector('#'+copycontent+htmltag.getAttribute('class')).innerText
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    })
+))
+
+let hashcopy = document.querySelectorAll('button.btn.hashcopy').forEach(eee => (
+    eee.addEventListener("click", () => {
+        const copycontent = eee.getAttribute('copy')
+        const str = window.location.origin+'/assets/'+'redirects/'+copycontent+'.html'
+
+
+        const el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    })
+))
+
+let Switchtype = document.querySelectorAll('button.Switchtype').forEach(ee => (
+    ee.addEventListener("click", () => {
+        htmltag.classList.toggle('css')
+        htmltag.classList.toggle('scss')
+        if(htmltag.getAttribute('class') == 'css'){
+            ee.innerText = "scss"
+        } else {
+            ee.innerText = "css"
+        }
+    })
+))
+function redirect(){
+    window.location = window.location.origin+'/snippets.html#'+window.location.pathname.replace('/assets/redirects','').replace('.html','')
 }
