2a1587a0052975f15cd44014d6d5d3cddf15c6f4
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-07-17 12:26:57 -0700

    fixes

--- assets/js.js
+++ assets/js.js
@@ -72,19 +72,11 @@ function getsnippets(){
         cache: "reload",
     }).then(response=>response.json()).then(data=>{
         for (const item of document.querySelectorAll('.snippets:not(.multi)')) {
-            item.children[0].innerHTML = '<div>' + data[item.getAttribute('id')].name +  `</div><div class="get_url">
-            <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-                <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
-            </svg>
-        </div>`
+            item.children[0].innerHTML = `<div> ${data[item.getAttribute('id')].name} | ${data[item.getAttribute('id')].author} </div><div class="get_url"><svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></div>`
             item.children[1].children[0].innerText = data[item.getAttribute('id')][document.querySelector('html').getAttribute('codetype')]
         }
         for (const item of document.querySelectorAll('.snippets.multi')) {
-            item.children[0].innerHTML = '<div>' + data[item.getAttribute('id')].name +  `</div><div class="get_url">
-            <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-                <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
-            </svg>
-        </div>`
+            item.children[0].innerHTML = `<div> ${data[item.getAttribute('id')].name} | ${data[item.getAttribute('id')].author} </div><div class="get_url"><svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></div>`
             for (const item2 of document.querySelectorAll('.snippets.multi>div')) {
                 item2.children[0].innerText = data[item.getAttribute('id')][item2.getAttribute('id')].name
                 item2.children[1].children[0].innerText = data[item.getAttribute('id')][item2.getAttribute('id')][document.querySelector('html').getAttribute('codetype')]
@@ -169,7 +161,7 @@ const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
 function locationHashChanged() {
     if(window.location.hash !== '') {
-        if ( document.querySelector('.snippets#'+window.location.hash.replace('#','')) ) {
+        if ( document.querySelector('.snippets'+window.location.hash) ) {
             document.querySelector('main').classList.add('hashed')
             document.querySelector('.snippets'+window.location.hash).classList.add('hashed')
             setTimeout(() => {
@@ -177,9 +169,7 @@ function locationHashChanged() {
             }, 30);
         }
     } else{
-        console.log('success pt1')
         if( window.location.hash === '' && document.querySelector('main.hashed')){
-            console.log('success pt2')
             document.querySelector('main.hashed').classList.remove('hashed')
             document.querySelector('.snippets.hashed').classList.remove('hashed')
         }
