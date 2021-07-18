bb8eb03520b4908819fcef05dc157102a06d2034
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-07-17 23:55:40 -0700

    fixes

--- assets/js.js
+++ assets/js.js
@@ -38,7 +38,7 @@ if(document.querySelector('#savecustomcss')){
     document.querySelector('#savecustomcss').addEventListener("click", () => {
         localStorage.setItem('customcss', document.querySelector('#customcss').value)   
         if(!document.querySelector('#stylecustomcss')) {
-            document.querySelector('head').innerHTML += `<style id="stylecustomcss">`+localStorage.getItem("customcss")+`</style>`
+            document.querySelector('head').innerHTML += `<style id="stylecustomcss">${localStorage.getItem("customcss")}</style>`
         }
         if(document.querySelector('#stylecustomcss')) {
             document.querySelector('#stylecustomcss').innerHTML  = localStorage.getItem("customcss")
@@ -46,16 +46,14 @@ if(document.querySelector('#savecustomcss')){
     })
 }
 if( localStorage.getItem("customcss") ){
-    document.querySelector('head').innerHTML += `<style id="stylecustomcss">`+localStorage.getItem("customcss")+`</style>`
+    document.querySelector('head').innerHTML += `<style id="stylecustomcss">${localStorage.getItem("customcss")}</style>`
     if (document.querySelector('#customcss')){
         document.querySelector('#customcss').value = localStorage.getItem("customcss")
     }
 }
 if(document.querySelector('#clearcustomcss')){
     document.querySelector('#clearcustomcss').addEventListener("click", () => {
-        localStorage.setItem('customcss', `html > body > #appmount > main{
-    padding-right: 50px;
-}`)
+        localStorage.setItem('customcss', ``)
         document.querySelector('#customcss').value = ''
         if( document.querySelector('#stylecustomcss') ){
             document.querySelector('#stylecustomcss').remove()
@@ -71,10 +69,10 @@ function getsnippets(){
         mode: "same-origin",
         cache: "reload",
     }).then(response=>response.json()).then(data=>{
-        for (const item of document.querySelectorAll('.snippets')) {
-            item.children[0].innerHTML = `<div> ${data[item.getAttribute('id')].name} | ${data[item.getAttribute('id')].author} </div><div class="get_url"><svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></div>`
-            item.children[1].children[0].innerText = data[item.getAttribute('id')][document.querySelector('html').getAttribute('codetype')]
-        }
+        document.querySelector('html[snippets] main').innerHTML = ''
+        Object.keys(data).forEach(key => {
+            document.querySelector('html[snippets] main').innerHTML += `<div class="snippets" id="${key}"><h2><div> ${data[key].name} | ${data[key].author} </div><div class="get_url"><svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></div></h2><pre><code>${data[key][document.querySelector('[snippets]').getAttribute('codetype')]}</code></pre><button class="copy_code">copy</button></div>`
+        });
     })
 }
 if(document.querySelector('#switchcodetype')){
@@ -134,7 +132,7 @@ const callback = function(mutationsList, observer) {
             if(document.querySelector('.get_url>svg')){
                 for (const item of document.querySelectorAll('.get_url>svg')) {
                     item.addEventListener("click", () => {
-                        const str = window.location.origin + '/snippets/#' + item.parentElement.parentElement.parentElement.getAttribute('id')
+                        const str = window.location.origin + `/snippets/#${item.parentElement.parentElement.parentElement.getAttribute('id')}`
                         const el = document.createElement('textarea');
                         el.value = str;
                         el.setAttribute('readonly', '');
@@ -156,9 +154,9 @@ function locationHashChanged() {
     if(window.location.hash !== '') {
         if ( document.querySelector('.snippets'+window.location.hash) ) {
             document.querySelector('main').classList.add('hashed')
-            document.querySelector('.snippets'+window.location.hash).classList.add('hashed')
+            document.querySelector(`.snippets ${window.location.hash}`).classList.add('hashed')
             setTimeout(() => {
-                document.title = document.querySelector(window.location.hash).children[0].children[0].innerText + ' | DB'
+                document.title = `${document.querySelector(window.location.hash).children[0].children[0].innerText} | DB`
             }, 30);
         }
     } else{
