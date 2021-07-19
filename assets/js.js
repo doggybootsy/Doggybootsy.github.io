c83621a267ec49e7d4055aebd99f35e3a3425ee0
doggybootsy <71196819+doggybootsy@users.noreply.github.com>
2021-07-18 18:51:04 -0700

    hljs

--- assets/js.js
+++ assets/js.js
@@ -71,9 +71,27 @@ function getsnippets(){
     }).then(response=>response.json()).then(data=>{
         document.querySelector('html[snippets] main').innerHTML = ''
         Object.keys(data).forEach(key => {
-            document.querySelector('html[snippets] main').innerHTML += `<div class="snippets" id="${key}"><h2><div> ${data[key].name} | ${data[key].author} </div><div class="get_url"><svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path></svg></div></h2><pre><code>${data[key][document.querySelector('[snippets]').getAttribute('codetype')]}</code></pre><button class="copy_code">copy</button></div>`
+            document.querySelector('html[snippets] main').innerHTML += `
+<div class="snippets" id="${key}">
+    <h2>
+        <div> ${data[key].name} | ${data[key].author} </div>
+        <div class="get_url">
+            <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+                <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
+            </svg>
+        </div>
+    </h2>
+
+    <pre><code class="language-${document.querySelector('[snippets]').getAttribute('codetype')}">${data[key][document.querySelector('[snippets]').getAttribute('codetype')]}</code></pre>
+
+    <button class="copy_code">copy</button>
+</div>
+`
         });
-    })
+        hljs.highlightAll();
+    }).catch(function() {
+        document.querySelector('html[snippets] main').innerHTML += `Unable to connect to <a href="https://doggybootsy.github.io/snippets.json">https://doggybootsy.github.io/snippets.json</a>`
+    });
 }
 if(document.querySelector('#switchcodetype')){
     document.querySelector('#switchcodetype').addEventListener("click", () => {
